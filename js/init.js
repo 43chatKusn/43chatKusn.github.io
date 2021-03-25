08b29beeb2c07ecc11d18d43e86a83be14b2193e
43chatKusn <81338279+43chatKusn@users.noreply.github.com>
2021-03-24 19:57:58 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,5 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
+    apiKey: "AIzaSyCWgx0Jcjq9ILREx_RD2DQ77Z-zaBgZNrI",
+    authDomain: "chatkusn.firebaseapp.com",
+    projectId: "chatkusn",
+    storageBucket: "chatkusn.appspot.com",
+    messagingSenderId: "318120468680",
+    appId: "1:318120468680:web:84cd1565620abe844045fd",
+    measurementId: "G-BCMZ8D72LF"
     
     });
